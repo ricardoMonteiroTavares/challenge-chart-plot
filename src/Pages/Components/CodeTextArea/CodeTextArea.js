fd2f4f4aaa3f7f89e33c0f99e94d10773f2d9221
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-05 11:52:16 -0300

    Create component CodeTextArea

---
+++ src/Pages/Components/CodeTextArea/CodeTextArea.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CodeTextArea.css';
+
+export default class CodeTextArea extends React.Component{
+    render(){
+        return(
+            <div>
+                <textarea className="textArea">
+                </textarea>
+            </div>
+        );
+    }
+}
