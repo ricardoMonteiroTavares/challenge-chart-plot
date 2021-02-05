e20927c3080ddc659e219dcc1584525b3d514a5d
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-04 22:12:40 -0300

    Create Bottom

---
+++ src/Pages/Components/Bottom/Bottom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Bottom.css';
+
+export default class Bottom extends React.Component{
+  render(){
+    return(
+      <div className="bottom">
+        <button className="button" onClick="">GENERATE CHART</button>
+      </div>
+    );
+  }
+}
