1a9dee553e249b85f5feb39663c47a380b2da173
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-05 11:52:30 -0300

    Import CodeTextArea

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header/Header.js';
 import Bottom from './Components/Bottom/Bottom.js';
+import CodeTextArea from "./Components/CodeTextArea/CodeTextArea.js";
 
 function App() {
   return (
     <div>
      <Header/>
+     <CodeTextArea/>
      <Bottom/>
     </div>
   );
