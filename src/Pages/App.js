1dd9af503ea570f378b3f0d2aad9ea2af34257d5
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-04 22:14:31 -0300

    Remove style in App and transfer location from App.js

---
+++ src/Pages/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Components/Header/Header.js';
+import Bottom from './Components/Bottom/Bottom.js';
+
+function App() {
+  return (
+    <div>
+     <Header/>
+     <Bottom/>
+    </div>
+  );
+}
+
+export default App;
