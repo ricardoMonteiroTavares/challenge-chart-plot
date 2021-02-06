53af97e0fcb40d2535227371afd9a257496a6ddd
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-06 00:58:50 -0300

    Remove textarea tag and addition Line object

--- src/Pages/Components/CodeTextArea/CodeTextArea.js
+++ src/Pages/Components/CodeTextArea/CodeTextArea.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import Line from './Line/Line';
 import './CodeTextArea.css';
 
 export default class CodeTextArea extends React.Component{
     render(){
         return(
-            <div>
-                <textarea className="textArea">
-                </textarea>
-            </div>
+            <div className="textArea">
+                <Line />
+            </div>            
         );
     }
 }
