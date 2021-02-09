5e2edb51c9368488f3f0a2224c247ead6e3d1600
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-08 21:43:20 -0300

    Addition of the height variable in the Code Area class

--- src/Pages/Components/CodeTextArea/CodeTextArea.js
+++ src/Pages/Components/CodeTextArea/CodeTextArea.js
@@ -3,9 +3,14 @@ import Line from './Components/Line/Line';
 import './CodeTextArea.css';
 
 export default class CodeTextArea extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
     render(){
+        const style = {height: this.props.height+'px'}
         return(
-            <div className="textArea">
+            <div style={style} className="textArea">
                 <Line />
             </div>            
         );
