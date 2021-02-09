49357e21e82d5e13da18e436ea55eed244173a50
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-08 21:45:10 -0300

    Addition of mouse events to readjust the height of the Code Area class in the App class

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Components/Header/Header.js';
 import Bottom from './Components/Bottom/Bottom.js';
 import CodeTextArea from "./Components/CodeTextArea/CodeTextArea.js";
+import Resizer from './Components/Resizer/Resizer';
 
-function App() {
-  return (
-    <div>
-     <Header/>
-     <CodeTextArea/>
-     <Bottom/>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      codeTextAreaHeight: 400,
+      resizing: false,
+    };
+  }
+  mouseDownHandler = e => {
+      this.setState(({
+        resizing: true,   
+      }), () => {
+        window.addEventListener("mousemove", this.resize, false);
+        window.addEventListener("mouseup", this.stopResize, false);
+      });
+  }
+
+  resize = event => {
+    const mousePositionY = event.clientY;
+    this.setState({
+      codeTextAreaHeight: (mousePositionY - 66)
+    });
+  }
+
+  stopResize = event => {
+    console.log('stop');
+    this.setState(({
+      resizing: false
+    }));
+    window.removeEventListener("mousemove", this.resize, false);
+    window.removeEventListener("mouseup", this.stopResize, false);
+  }
+  
+
+  render(){
+    return (
+      <div>
+       <Header/>
+       <CodeTextArea height={this.state.codeTextAreaHeight}/>
+       <Resizer mouseDownHandler={this.mouseDownHandler}/>
+       <Bottom/>
+      </div>
+    );
+  }
+  
 }
-
-export default App;
