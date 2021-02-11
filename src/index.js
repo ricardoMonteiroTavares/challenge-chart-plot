b9d067947729218cbcb54d935afdbc49da08eb04
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-10 23:23:06 -0300

    Addition of a function that looks for the window size

--- src/index.js
+++ src/index.js
@@ -4,7 +4,41 @@ import './index.css';
 import App from './Pages/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+const Component = () => {
+    const { height, width } = useWindowDimensions();
+  
+    return (
+      <div>
+        width: {width} ~ height: {height}
+      </div>
+    );
+  }
+ReactDOM.render(<Component />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
